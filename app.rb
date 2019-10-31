d65488dc1f030bec396663dab919b39e7a0ad443
Dnpypy <denlatypoff@yandex.ru>
2019-10-31 18:16:57 +0300

    delete layout.erb, add welcome.erb, add form contact.erb

--- app.rb
+++ app.rb
@@ -3,14 +3,42 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
-# подгружаем библиотеку sqlite3
-require 'sqlite3'
-
 
 get "/" do
-  erb :layout
+  erb :index
 end
 
 get "/contact" do
-  erb :contact, :layout  => :contact
+  erb :contact
+end
+
+post "/contact" do
+
+    @login       = params[:login]
+    @email       = params[:email]
+    @psw         = params[:psw]
+    @psw_repeat  = params[:psw_repeat]
+
+
+    if @psw != @psw_repeat
+      @alert_psw = " <p style='color:red'>Введите повторный пароль!!!</p>"
+      erb :contact
+    else
+
+      erb :welcome
+
+    end
+
+    # if @login == "admin" and @password == "1"
+    #   # @check = @password.class
+    #   erb :welcome
+    # end
+
+    # redirect "/contact"
+
+end
+
+get "/welcome" do
+  @alert_psw = "Введите повторный пароль"
+  erb :welcome
 end
